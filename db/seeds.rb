d1d95b13efdaca03036783d6dbfe8a1c3bba8811
WWDavidson <w.w.davidson33@gmail.com>
2022-02-18 12:52:21 -0600

    add intial coins to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create([ 
+    {
+    coin_gecko_id: 'usd-coin',
+    name: 'USD Coin',
+    ticker: 'usdc',
+    logo: "https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png?1547042389",
+    price: 1.00,
+    price_change_24h: -0.000560419114,
+    price_change_percentage_24h: -0.05595
+    },
+    {
+    coin_gecko_id: 'bitcoin',
+    name: 'Bitcoin',
+    ticker: 'btc',
+    logo: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+    price: 40045,
+    price_change_24h: -1056.982212832365,
+    price_change_percentage_24h: -2.57162
+    },
+    {
+    coin_gecko_id: 'ethereum',
+    name: 'Ethereum',
+    ticker: 'eth',
+    logo:"https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
+    price: 2774.92,
+    price_change_24h: -131.0947565989,
+    price_change_percentage_24h: -4.51115
+    },
+    {
+    coin_gecko_id: 'binancecoin',
+    name: 'BNB',
+    ticker: 'bnb',
+    logo: "https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png?1644979850",
+    price: 396.63,
+    price_change_24h: -9.531828542664,
+    price_change_percentage_24h: -2.34678
+    },
+    {
+    coin_gecko_id: 'dogecoin',
+    name: 'Dogecoin',
+    ticker: 'doge',
+    logo: "https://assets.coingecko.com/coins/images/5/large/dogecoin.png?1547792256",
+    price: 0.138172,
+    price_change_24h: -0.002625331733,
+    price_change_percentage_24h: -1.86462
+    }   
+    
+])
+
