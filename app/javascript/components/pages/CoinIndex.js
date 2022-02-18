2a1d2a177d4839bd35d1a88a30bb013cf8670bb6
WWDavidson <w.w.davidson33@gmail.com>
2022-02-18 12:12:21 -0600

    add bootstrap and reactstrap; add initial components and pages

---
+++ app/javascript/components/pages/CoinIndex.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CoinIndex extends Component {
+  render() {
+    return (
+      <div>CoinIndex</div>
+    )
+  }
+}
---
+++ app/javascript/components/pages/Holdings.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Holdings extends Component {
+  render() {
+    return (
+      <div>Holdings</div>
+    )
+  }
+}
