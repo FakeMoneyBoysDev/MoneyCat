b28c5870f90bc457a8bd1e48fd251ca9c8ccac46
gitgood-ENT <ward.noahd@gmail.com>
2022-02-18 16:25:36 -0800

    fixed errors in migrations

--- db/migrate/20220217224133_rename_column.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumn < ActiveRecord::Migration[6.1]
-  def change
-    rename_column(:holdings, :coin_id, :coin_gecko_id)
-    # change_column(:holdings, :coin_id, :string)
-  end
-end
--- db/migrate/20220217225101_add_column.rb
+++
@@ -1,5 +0,0 @@
-class AddColumn < ActiveRecord::Migration[6.1]
-  def change
-    add_column(:holdings, :coin_gecko_id, :string)
-  end
-end
