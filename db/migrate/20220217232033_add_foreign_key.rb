a70f18ecf2e723f6e8dec66508170bf0934285e5
gitgood-ENT <ward.noahd@gmail.com>
2022-02-22 09:44:59 -0800

    deleted add foreign key migration

--- db/migrate/20220217232033_add_foreign_key.rb
+++
@@ -1,8 +0,0 @@
-class AddForeignKey < ActiveRecord::Migration[6.1]
-  def change
-    add_foreign_key :holdings, :users
-    add_foreign_key :holdings, :coins
-    add_foreign_key :transactions, :users
-    add_foreign_key :transactions, :coins
-  end
-end
