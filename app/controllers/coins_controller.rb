bf9ea44af3c8391ecd23453953c7e9e37edab8e5
LEARN Academy <ford.py@gmail.com>
2022-02-26 10:35:22 -0800

    Added the destroy method to coins controller.

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -24,8 +24,17 @@ class CoinsController < ApplicationController
         render json: coin
        else
         render json: coin.error
+       end
     end
-end
+    def destroy 
+        coin = Coin.find(params[:id])
+        coin.destroy()
+         if coin.valid?
+            render json: coin
+         else
+            render json: coin.error, status: 422
+         end
+    end
 private
   def coin_params
     params.require(:coins).permit(:name, :ticker, :logo, :price, :price_change_24h, :user_id, :price_change_percentage_24h)
