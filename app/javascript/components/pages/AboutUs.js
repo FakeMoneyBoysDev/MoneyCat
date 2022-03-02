ac122507b129160898a3b4b773135a5127fb8471
LEARN Academy <ford.py@gmail.com>
2022-03-02 11:46:29 -0800

    finsished styling about us

--- app/javascript/components/pages/AboutUs.js
+++ app/javascript/components/pages/AboutUs.js
@@ -6,33 +6,32 @@ export default class AboutUs extends Component {
       <>
         <h1>AboutUs</h1>
         <div className="bio">
-          <p>
-            Product Manager: A recent graduate of LEARN Academy, a full stack
-            web development bootcamp based in San Diego, California. He obtained
-            a degree in General Education in April of 2015 from Columbia College
-            Chicago. Demetrius has a past military background and was employed
-            in local government until his retirement in 2007. Demetrius is a
-            Florida resident and an active participant in a local javascript and
-            php meetup group. He's very passionate about blockchain, non
-            fungible tokens and web technologies. He is actively pursuing a
-            career in web development and has gained valuable skills in react,
-            javascript, ruby on rails, and postgresql.
-          </p>
+          <h2>Demetrius Ford</h2>
+          <h4>Product Manager</h4>A recent graduate of LEARN Academy, a full
+          stack web development bootcamp based in San Diego, California. He
+          obtained a degree in General Education in April of 2015 from Columbia
+          College Chicago. Demetrius has a past military background and was
+          employed in local government until his retirement in 2007. Demetrius
+          is a Florida resident and an active participant in a local javascript
+          and php meetup group. He's very passionate about blockchain, non
+          fungible tokens and web technologies. He is actively pursuing a career
+          in web development and has gained valuable skills in react,
+          javascript, ruby on rails, and postgresql.
         </div>
-        ;
+
         <div className="bio">
-          <p>
-            Project Manager: A recent graduate of LEARN Academy, a full stack
-            web development bootcamp based in San Diego, California. I have
-            honorably served in the US Navy as a Electronic Technician attended
-            naval nuclear power school He is actively pursuing a career in web
-            development and has valuable skills in react, javascript, ruby on
-            rails, and postgresql.
-          </p>
+          <h2>Noah Ward</h2>
+          <h4>Project Manager</h4>A recent graduate of LEARN Academy, a full
+          stack web development bootcamp based in San Diego, California. I have
+          honorably served in the US Navy as a Electronic Technician attended
+          naval nuclear power school He is actively pursuing a career in web
+          development and has valuable skills in react, javascript, ruby on
+          rails, and postgresql.
         </div>
-        ;
+
         <div className="bio">
-          Tech Lead: A recent graduate of LEARN Academy, a full stack web
+          <h2>Warren Davidson</h2>
+          <h4>Tech Lead</h4>A recent graduate of LEARN Academy, a full stack web
           development bootcamp based in San Diego, California. He obtained a
           degree in audio production from Los Angeles Recording School and
           honorably served in the US Navy Warren as a Aviation Electrician is a
@@ -41,8 +40,10 @@ export default class AboutUs extends Component {
           actively pursuing a career in web development and has gained valuable
           skills in react, javascript, ruby on rails, and postgresql.
         </div>
-        ;
+
         <div className="bio">
+          <h2>Pavan Singh</h2>
+          <h4>Design Lead</h4>
           Design Lead: A recent graduate of LEARN Academy, a full stack web
           development bootcamp based in San Diego, California. Who honorably
           served in the USMC as a diesel mechanic before transitioning to the
@@ -50,9 +51,8 @@ export default class AboutUs extends Component {
           servers at Naval Base Point Loma. Pavan is a San Diego resident and an
           active participant in a local Ruby and coding meetup group. He is
           actively pursuing a career in web development and has gained valuable
-          skills in react, javascript, ruby on rails, and postgresql
+          skills in react, javascript, ruby on rails, and postgresql.
         </div>
-        ;
       </>
     );
   }
